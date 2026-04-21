d7003a0c96ce8533530d4e7aa3407ed3055f9505
Allan Paul Sodsod <sodsodallanpaulii@gmail.com>
2026-04-21 13:29:13 +0800

    feat: implement auth layouts and create welcome landing page with animated background

--- resources/js/layouts/auth/auth-card-layout.tsx
+++ resources/js/layouts/auth/auth-card-layout.tsx
@@ -24,11 +24,9 @@ export default function AuthCardLayout({
             <div className="flex w-full max-w-md flex-col gap-6">
                 <Link
                     href={home()}
-                    className="flex items-center gap-2 self-center font-medium"
+                    className="flex items-center justify-center self-center"
                 >
-                    <div className="flex h-9 w-9 items-center justify-center">
-                        <AppLogoIcon className="size-9 fill-current text-black dark:text-white" />
-                    </div>
+                    <AppLogoIcon className="size-24" />
                 </Link>
 
                 <div className="flex flex-col gap-6">
