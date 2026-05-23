19384a92913953dc1cf8d64e7b6d2fa5b08d9c18
Mark Kenneth Nudo <kennethnudo27@gmail.com>
2026-05-24 01:00:58 +0800

    chore: revoke execute permissions from anon and public roles on rls_auto_enable function

---
+++ database/migrations/2026_05_24_005958_revoke_anon_execute_from_rls_auto_enable.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared("
+            DO $$
+            BEGIN
+                IF EXISTS (
+                    SELECT 1
+                    FROM pg_proc p
+                    JOIN pg_namespace n ON n.oid = p.pronamespace
+                    WHERE p.proname = 'rls_auto_enable'
+                      AND n.nspname = 'public'
+                ) THEN
+                    EXECUTE 'REVOKE EXECUTE ON FUNCTION public.rls_auto_enable() FROM anon, public';
+                END IF;
+            END $$;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared("
+            DO $$
+            BEGIN
+                IF EXISTS (
+                    SELECT 1
+                    FROM pg_proc p
+                    JOIN pg_namespace n ON n.oid = p.pronamespace
+                    WHERE p.proname = 'rls_auto_enable'
+                      AND n.nspname = 'public'
+                ) THEN
+                    EXECUTE 'GRANT EXECUTE ON FUNCTION public.rls_auto_enable() TO anon, public';
+                END IF;
+            END $$;
+        ");
+    }
+};
