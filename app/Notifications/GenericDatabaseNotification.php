50526ed65986027f1f45400b42f4347ce77ef564
Mark Kenneth Nudo <kennethnudo27@gmail.com>
2026-06-04 00:06:38 +0800

    Fix generic database notification structure and update session config

--- app/Notifications/GenericDatabaseNotification.php
+++ app/Notifications/GenericDatabaseNotification.php
@@ -3,9 +3,10 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class GenericDatabaseNotification extends Notification
+class GenericDatabaseNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
